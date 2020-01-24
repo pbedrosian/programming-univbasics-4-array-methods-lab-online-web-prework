8e52905870cae653545a92e8590461852ef26d89
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-01-24 18:50:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,30 +1,23 @@
 def using_include(array, element)
-groomies = ["aaray", "element"]
-groomies.include?("element")
+  array.include?(element)
 end
 
 def using_sort(array)
-  groomies = ["nick", "nathan", "ben"]
-  groomies.sort
+
 end
 
 def using_reverse(array)
-groomies = ["nick", "nathan", "ben", "arrays!"]
-groomies.reverse
+
 end
 
 def using_first(array)
-groomies = ["wow", "nick", "nathan", "ben"]
-groomies.first
+
 end
 
 def using_last(array)
-  groomies = ["nick", "nathan", "ben", "arrays!"]
-  groomies.last
 
 end
 
 def using_size(array)
-groomies = ["wow", "nick", "nathan", "ben", "test", "hello world"]
-groomies.size
+
 end
